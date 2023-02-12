6067f0843b20d6b39519ecc580c90ac4bb00cded
unknown <3535011@alu.murciaeduca.es>
2023-02-12 21:20:30 +0100

    version 005

--- libs/setup.php
+++ libs/setup.php
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<?php
-// Obtiene la instancia del objeto que guarda los datos de configuración
-$config = Config::singleton();
-
-// Carpetas para los Controladores, los Modelos y las Vistas
-$config->set('controllersFolder', 'controllers/');
-$config->set('modelsFolder', 'models/');
-$config->set('viewsFolder', 'views/');
-
-// Parámetros de conexión a la BD
-$config->set('dbhost', 'containers-us-west-127.railway.app');
-$config->set('dbname', 'railway');
-$config->set('dbuser', 'root');
-$config->set('dbpass', 'Qvf0EM96tyAIeaEwu4Hg');
-?>
-=======
 <?php
 // Obtiene la instancia del objeto que guarda los datos de configuración
 $config = Config::singleton();
@@ -30,4 +13,3 @@ $config->set('dbname', 'railway');
 $config->set('dbuser', 'root');
 $config->set('dbpass', 'Qvf0EM96tyAIeaEwu4Hg');
 ?>
->>>>>>> 6cd564c5c0fc1bd7eef27bdc1a3e882ae79b69b4
