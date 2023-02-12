6cd564c5c0fc1bd7eef27bdc1a3e882ae79b69b4
MolinaGarciaJose <101943461+MolinaGarciaJose@users.noreply.github.com>
2023-02-12 21:05:09 +0100

    Update setup.php

--- libs/setup.php
+++ libs/setup.php
@@ -8,8 +8,8 @@ $config->set('modelsFolder', 'models/');
 $config->set('viewsFolder', 'views/');
 
 // Parámetros de conexión a la BD
-$config->set('dbhost', 'localhost');
-$config->set('dbname', 'dwespruebas');
-$config->set('dbuser', 'super');
-$config->set('dbpass', '123456');
+$config->set('dbhost', 'containers-us-west-127.railway.app');
+$config->set('dbname', 'railway');
+$config->set('dbuser', 'root');
+$config->set('dbpass', 'Qvf0EM96tyAIeaEwu4Hg');
 ?>
